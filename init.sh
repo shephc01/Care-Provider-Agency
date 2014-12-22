bb25f567becc2373aa25b47fca53e840e74cd856
schenget <schenget@redhat.com>
2014-12-22 17:21:20 +0000

    placeholder for the care provider demo showcasing case management in BPMS

---
+++ init.sh
@@ -0,0 +1,106 @@
+#!/bin/sh 
+DEMO="Care Provider Agency Demo"
+AUTHORS="Shepherd Chengeta"
+PROJECT="https://github.com/shephc01/Care-Provider-Agency"
+PRODUCT="JBoss BPM Suite"
+JBOSS_HOME=./target/jboss-eap-6.1
+SERVER_DIR=$JBOSS_HOME/standalone/deployments/
+SERVER_CONF=$JBOSS_HOME/standalone/configuration/
+SERVER_BIN=$JBOSS_HOME/bin
+SRC_DIR=./installs
+SUPPORT_DIR=./support
+PRJ_DIR=./projects
+BPMS=jboss-bpms-installer-6.0.3.GA-redhat-1.jar
+VERSION=6.0.3
+
+# wipe screen.
+clear 
+
+echo
+echo "#################################################################"
+echo "##                                                             ##"   
+echo "##  Setting up the ${DEMO}                          ##"
+echo "##                                                             ##"   
+echo "##                                                             ##"   
+echo "##     ####  ####   #   #      ### #   # ##### ##### #####     ##"
+echo "##     #   # #   # # # # #    #    #   #   #     #   #         ##"
+echo "##     ####  ####  #  #  #     ##  #   #   #     #   ###       ##"
+echo "##     #   # #     #     #       # #   #   #     #   #         ##"
+echo "##     ####  #     #     #    ###  ##### #####   #   #####     ##"
+echo "##                                                             ##"   
+echo "##                                                             ##"   
+echo "##  brought to you by,                                         ##"   
+echo "##   ${AUTHORS}          ##"
+echo "##                                                             ##"   
+echo "##  ${PROJECT}       ##"
+echo "##                                                             ##"   
+echo "#################################################################"
+echo
+
+command -v mvn -q >/dev/null 2>&1 || { echo >&2 "Maven is required but not installed yet... aborting."; exit 1; }
+
+# make some checks first before proceeding.	
+if [[ -r $SRC_DIR/$BPMS || -L $SRC_DIR/$BPMS ]]; then
+		echo Product sources are present...
+		echo
+else
+		echo Need to download $BPMS package from the Customer Portal 
+		echo and place it in the $SRC_DIR directory to proceed...
+		echo
+		exit
+fi
+
+# Move the old JBoss instance, if it exists, to the OLD position.
+if [ -x $JBOSS_HOME ]; then
+		echo "  - existing JBoss product install removed..."
+		echo
+		rm -rf target
+fi
+
+# Run installer.
+echo Product installer running now...
+echo
+java -jar $SRC_DIR/$BPMS $SUPPORT_DIR/installation-bpms -variablefile $SUPPORT_DIR/installation-bpms.variables
+
+echo "  - enabling demo accounts role setup in application-roles.properties file..."
+echo
+cp $SUPPORT_DIR/application-roles.properties $SERVER_CONF
+
+echo "  - setting up demo projects..."
+echo
+cp -r $SUPPORT_DIR/bpm-suite-demo-niogit $SERVER_BIN/.niogit
+
+
+echo "  - deploying external-client-ui-form-1.0.war to EAP deployments directory"
+cp -r $PRJ_DIR/external-client-ui-form/target/external-client-ui-form-1.0.war $SERVER_DIR/
+
+echo
+echo "  - setting up standalone.xml configuration adjustments..."
+echo
+cp $SUPPORT_DIR/standalone.xml $SERVER_CONF
+
+echo "  - making sure standalone.sh for server is executable..."
+echo
+chmod u+x $JBOSS_HOME/bin/standalone.sh
+
+# Optional: uncomment this to install mock data for BPM Suite.
+#
+#echo - setting up mock bpm dashboard data...
+#cp $SUPPORT_DIR/1000_jbpm_demo_h2.sql $SERVER_DIR/dashbuilder.war/WEB-INF/etc/sql
+#echo
+
+echo
+echo "========================================================================"
+echo "=                                                                      ="
+echo "=  You can now start the $PRODUCT with:                         ="
+echo "=                                                                      ="
+echo "=   $SERVER_BIN/standalone.sh                           ="
+echo "=                                                                      ="
+echo "=  Login into business central at:                                     ="
+echo "=                                                                      ="
+echo "=    http://localhost:8080/business-central  (u:erics / p:bpmsuite1!)  ="
+echo "=                                                                      ="
+echo "=                                                                      ="
+echo "=  $PRODUCT $VERSION $DEMO Setup Complete.            ="
+echo "=                                                                      ="
+echo "========================================================================"
